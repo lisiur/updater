e2665e67f8f1f0c4ff5db2742eb95c838dbd03cb
lisiur <lisiurday@gmail.com>
2023-03-12 18:19:09 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { checkNewVersion } from "./updater";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
 async function greet() {
   checkNewVersion()
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
   document
     .querySelector("#greet-button")
     ?.addEventListener("click", () => greet());
